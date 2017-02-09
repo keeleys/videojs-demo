ac73c9e84973e754205191b7d5d6ed6f7d90b8a2
keeleys <keeley.jun@qq.com>
2017-02-09 14:12:09 +0800

    add docs

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,21 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: '[name]-[hash:6].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-      loaders: [
-          { test: /\.css$/, loader: 'style-loader!css-loader' },
-          { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
-          { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader'}
-      ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ 
-      template: 'src/index.html'
-    })
-  ]
+    entry: './src/main.js',
+    output: {
+        filename: '[name]-[hash:6].js',
+        path: path.resolve(__dirname, 'docs')
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
+            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader'}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
 };
